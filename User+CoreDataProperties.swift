7a337564dd2e97982983e84f66483d13bc05cf33
westcallista <77295181+westcallista@users.noreply.github.com>
2021-03-10 09:21:05 -0800

    Create User+CoreDataProperties.swift

---
+++ User+CoreDataProperties.swift
@@ -0,0 +1,55 @@
+//
+//  User+CoreDataProperties.swift
+//  Fetch
+//
+//  Created by Ronny on 2/25/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class User: NSManagedObject, Identifiable {
+    @NSManaged public var breed: String
+    @NSManaged public var name: String
+    @NSManaged public var dogName: String
+    @NSManaged public var created: Date?
+    @NSManaged public var city: String
+    @NSManaged public var curDogbreed: String
+    @NSManaged public var point: Int16
+
+}
+
+extension User {
+    static func getAllUsers() -> NSFetchRequest<User> {
+        let request: NSFetchRequest<User> = User.fetchRequest() as! NSFetchRequest<User>
+        
+//        let sortDescriptor = NSSortDescriptor(key: "created", ascending: true)
+//
+//        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
+
+public class CurrentUser: NSManagedObject, Identifiable {
+    @NSManaged public var name: String
+    @NSManaged public var created: Date
+    @NSManaged public var dogbreed: String
+    @NSManaged public var dogname: String
+    @NSManaged public var city: String
+    
+}
+
+extension CurrentUser {
+    static func getAllUsers() -> NSFetchRequest<CurrentUser> {
+        let request: NSFetchRequest<CurrentUser> = CurrentUser.fetchRequest() as! NSFetchRequest<CurrentUser>
+        
+//        let sortDescriptor = NSSortDescriptor(key: "created", ascending: true)
+//
+//        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
